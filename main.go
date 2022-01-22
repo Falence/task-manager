c3c94a92bc05247ecfb5cff69719daa3e15376af
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-22 18:34:39 +0100

    setup entry point of our program

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/falence/taskmanager/common"
+	"github.com/falence/taskmanager/routers"
+)
+
+func main() {
+	// Calls startup logic
+	common.StartUp()
+	// Get the mux router object
+	router := routers.InitRoutes()
+	// Create a negroni instance
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	server := &http.Server{
+		Addr: common.AppConfig.Server,
+		Handler: n,
+	}
+	log.Println("Listening...")
+	server.ListenAndServe()
+}
