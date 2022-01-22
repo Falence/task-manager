2332d553a0ab357f3307f9f2b0a247ae262f9b92
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-22 18:20:54 +0100

    adding indexes in mongodb

--- common/mongoUtils.go
+++ common/mongoUtils.go
@@ -37,3 +37,47 @@ func createDbSession() {
 		log.Fatalf("[createDbSession]: %s\n", err)
 	}
 }
+
+
+// Add indexes into MongoDB
+func addIndexes() {
+	var err error
+	userIndex := mgo.Index {
+		Key: []string{"email"},
+		Unique: true,
+		Background: true,
+		Sparse: true,
+	}
+	taskIndex := mgo.Index {
+		Key: []string{"createdby"},
+		Unique: true,
+		Background: true,
+		Sparse: true,
+	}
+	noteIndex := mgo.Index {
+		Key: []string{"taskid"},
+		Unique: true,
+		Background: true,
+		Sparse: true,
+	}
+
+	// Add indexes into MongoDB
+	session := GetSession().Copy()
+	defer session.Close()
+	userCol := session.DB(AppConfig.Database).C("users")
+	taskCol := session.DB(AppConfig.Database).C("tasks")
+	noteCol := session.DB(AppConfig.Database).C("notes")
+
+	err = userCol.EnsureIndex(userIndex)
+	if err != nil {
+		log.Fatalf("[addIndexes]: %s\n", err)
+	}
+	err = taskCol.EnsureIndex(taskIndex)
+	if err != nil {
+		log.Fatalf("[addIndexes]: %s\n", err)
+	}
+	err = noteCol.EnsureIndex(noteIndex)
+	if err != nil {
+		log.Fatalf("[addIndexes]: %s\n", err)
+	}
+}
