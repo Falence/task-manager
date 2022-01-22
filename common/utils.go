f10d3a9d16e054d0708ecc52fbf786226dc7e51d
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-22 17:38:04 +0100

    initializing appConfig

---
+++ common/utils.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type configuration struct {
+	Server, MongoDBHost, DBUser, DBPwd, Database string
+}
+
+// AppConfig holds the configuration values from config.json file
+var AppConfig configuration
+
+// Initialize AppConfig
+func initConfig() {
+	loadAppConfig()
+}
+
+// Reads config.json and decode into AppConfig
+func loadAppConfig() {
+	file, err := os.Open("common/config.json")
+	if err != nil {
+		log.Fatalf("[loadConfig]: %s\n", err)
+	}
+	defer file.Close()
+	err = json.NewDecoder(file).Decode(&AppConfig)
+	if err != nil {
+		log.Fatalf("[loadAppConfig]: %s\n", err)
+	}
+}
