34dee3bad5f6ab530d1a444214ef3452939cefc6
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-24 19:19:00 +0100

    implement context structure and its methods

---
+++ controllers/context.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"gopkg.in/mgo.v2"
+	"github.com/falence/taskmanager/common"
+)
+
+// Struct used for maitaining HTTP Request Context
+type Context struct {
+	MongoSession *mgo.Session
+}
+
+// Close mgo.Session
+func (c *Context) Close() {
+	c.MongoSession.Close()
+}
+
+// Return mgo.collection for the given name
+func (c *Context) DbCollection(name string) *mgo.Collection {
+	return c.MongoSession.DB(common.AppConfig.Database).C(name)
+}
+
+// Create a new Context object for each HTTP request
+func NewContext() *Context {
+	session := common.GetSession().Copy()
+	context := &Context{
+		MongoSession: session,
+	}
+	return context
+}
