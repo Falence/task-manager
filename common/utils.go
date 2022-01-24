2d2e2dd725a53b36824483dbc8e8399a5322390e
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-24 19:00:35 +0100

    implement helper function for displaying errors

--- common/utils.go
+++ common/utils.go
@@ -3,12 +3,27 @@ package common
 import (
 	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 )
 
 type configuration struct {
 	Server, MongoDBHost, DBUser, DBPwd, Database string
 }
+
+// Error types
+type (
+	appError struct {
+		Error string `json:"error"`
+		Message string `json:"message"`
+		HttpStatus int `json:"status"`
+	}
+
+	errorResource struct {
+		Data appError `json:"data"`
+	}
+)
+
 
 // AppConfig holds the configuration values from config.json file
 var AppConfig configuration
@@ -30,5 +45,20 @@ func loadAppConfig() {
 	err = json.NewDecoder(file).Decode(&AppConfig)
 	if err != nil {
 		log.Fatalf("[loadAppConfig]: %s\n", err)
+	}
+}
+
+// Display error
+func DisplayAppError(w http.ResponseWriter, handlerError error, message string, code int) {
+	errObj := appError{
+		Error: handlerError.Error(), // string value of the error object
+		Message: message,
+		HttpStatus: code,
+	}
+	log.Printf("[AppError]: %s\n", handlerError)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	if j, err := json.Marshal(errorResource{Data: errObj}); err == nil {
+		w.Write(j)
 	}
 }
