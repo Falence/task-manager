6b287b82fa53fc12ebd101f9ebd05d3194ed62d9
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-25 19:36:23 +0100

    implement taskRepository scaffolding

---
+++ data/taskRepository.go
@@ -0,0 +1,26 @@
+package data
+
+import (
+	"time"
+
+	"github.com/falence/taskmanager/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type TaskRepository struct {
+	C *mgo.Collection
+}
+
+func (r *TaskRepository) Create(task *models.Task) error {}
+
+func (r *TaskRepository) Update(task *models.Task) error {}
+
+func (r *TaskRepository) Delete(id string) error {}
+
+func (r *TaskRepository) GetAll() []models.Task {}
+
+func (r *TaskRepository) GetById(id string) (task models.Task, err error) {}
+
+func (r *TaskRepository) GetByUser(user string) []models.Task {}
+
