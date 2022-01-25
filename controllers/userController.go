7b86be9b6d802ed2efa5c901df53a2e07c8c26a8
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-25 17:50:24 +0100

    implement handler for /users/login route

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"context"
 	"encoding"
 	"encoding/json"
 	"net/http"
@@ -47,6 +48,75 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
+		w.Write(j)
+	}
+}
+
+// Handler for HTTP Post - "/users/login"
+// Authenticate with username and password
+func Login(w http.ResponseWriter, r *http.Request) {
+	var dataResource LoginResource
+	var token string
+	// Decode the incoming Login json
+	err := json.NewDecoder(r.Body).Decode(&dataResource)
+	if err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"Invalid Login data",
+			500,
+		)
+		return
+	}
+
+	loginModel := dataResource.Data
+	loginUser := models.User{
+		Email: loginModel.Email,
+		Password: loginModel.Password,
+	}
+	context := NewContext()
+	defer context.Close()
+	c := context.DbCollection("users")
+	repo := &data.UserRepository{c}
+
+	// Authenticate the login user
+	if user, err := repo.Login(loginUser); err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"Invalid login credentials",
+			401,
+		)
+		return
+	} else {
+		// Generate JWT token
+		token, err = common.GenerateJWT(user.Email, "member")
+		if err != nil {
+			common.DisplayAppError(
+				w,
+				err,
+				"Error while generating the access token",
+				500,
+			)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		user.HashPassword = nil
+		authUser := AuthUserModel{
+			User: user,
+			Token: token,
+		}
+		j, err := json.Marshal(AuthUserResource{Data: authUser})
+		if err != nil {
+			common.DisplayAppError(
+				w,
+				err,
+				"An unexpected error has occurred",
+				500,
+			)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 		w.Write(j)
 	}
 }
